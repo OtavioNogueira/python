2f77a61de3db373938c45e6eaae98562a3e45985
Oávio Nogueira <otavioa445@gmail.com>
2023-01-03 14:10:53 -0300

    d1

---
+++ d1.py
@@ -0,0 +1,16 @@
+import antigravity
+
+
+n = int(input('Insira um valor: '))
+
+ant = n-1
+su = n+1
+d = n*2
+t = n*3
+r = n**0.5 
+
+print('O antecessor desse valor é {}'.format(ant))
+print('O sucessor desse valor é {}'.format(su))
+print('O dobro desse valor é {}'.format(d))
+print('O triplo desse valor é {}'.format(t))
+print('A raiz desse valor é {}'.format(r))
