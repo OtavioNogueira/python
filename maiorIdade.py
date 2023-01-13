e22d7171c183b059c7afc02ffb0d56cd83a20ccf
Oávio Nogueira <otavioa445@gmail.com>
2023-01-13 11:47:46 -0300

    Maior Idade

--- maiorIdade.py
+++ maiorIdade.py
@@ -2,19 +2,17 @@ from datetime import date
 presente = date.today().year 
 
 aux = []
-aux2 = 0
+
 for i in range (1,3):
   nasc = int(input('Insira seu ano de nascimento: '))
   aux.append(nasc)
 
-for i in range (1,3):
-  print('{}'.format(aux[aux2]))
-  if presente - nasc > 18:
-    print('Maior de idade!')
-  elif presente - nasc < 18: 
-    print('Menor de idade! ')
-  aux2 += 1
-
+for i in range (2):
+  idade = presente - aux[i]
+  if idade >= 18:
+    print('O ano de nascimento é {}. Logo, maior de idade!'.format(aux[i]))
+  elif idade < 18: 
+    print('O ano de nascimento é {}. Logo, menor de idade!'.format(aux[i])) 
 
   
 
