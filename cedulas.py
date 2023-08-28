9ab9f3ceb40b4ef7ffed89ec7ac808365cadf638
OtavioNogueira <63080249+OtavioNogueira@users.noreply.github.com>
2023-08-28 18:31:24 -0300

    Update cedulas.py

--- cedulas.py
+++ cedulas.py
@@ -1,19 +1,31 @@
+valor = float(input())
 
-print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-valor = int(input('Insira o valor: '))
+cem = valor//100 
+cem2 = valor%100 
 
-ci = int(valor / 50)
-ci2 = valor % 50 
+cin = cem2//50
+cin2 = cem2%50
 
-vi = int(ci2/20)
-vi2 = (ci2 % 20)
+vin = cin2//20 
+vin2 = cin2%20
 
-de = int((vi2) / 10)
-de2 = (vi2 % 10)
+de = vin2//10 
+de2 = vin2%10
 
-um = int(de2 / 1)
+ci = de2//5 
+ci2 = de2%5
 
-print(f'Esse é o total de notas de R$50: {ci}')
-print(f'Esse é o total de notas de R$20: {vi}')
-print(f'Esse é o total de notas de R$10: {de}')
-print(f'Esse é o total de notas de R$1: {um}')
+do = ci2//2
+do2 = ci2%2
+
+um = ci2//1
+
+
+
+print('{:.0f} nota(s) de R$100'.format(cem))
+print('{:.0f} nota(s) de R$50'.format(cin))
+print('{:.0f} nota(s) de R$20'.format(vin))
+print('{:.0f} nota(s) de R$10'.format(de))
+print('{:.0f} nota(s) de R$5'.format(ci))
+print('{:.0f} nota(s) de R$2'.format(do))
+print('{:.0f} nota(s) de R$1'.format(um))
