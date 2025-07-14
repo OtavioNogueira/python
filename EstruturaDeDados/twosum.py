a3d4ffb53bfb19010be47e937545ac77ece12717
OtavioNogueira <63080249+OtavioNogueira@users.noreply.github.com>
2025-07-14 17:10:01 -0300

    Update twosum.py

--- EstruturaDeDados/twosum.py
+++ EstruturaDeDados/twosum.py
@@ -19,7 +19,7 @@ def partition(arr, left, right):
     pivot = arr[right]
     i = left - 1
     for j in range(left, right):
-        if arr[j][0] <= pivot[0]:  # Comparando pelo valor
+        if arr[j][0] <= pivot[0]:  
             i += 1
             arr[i], arr[j] = arr[j], arr[i]
     arr[i + 1], arr[right] = arr[right], arr[i + 1]
